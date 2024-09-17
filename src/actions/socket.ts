5eacda561b0643984b11e2fe5e0a4380e0d1e3b9
DouaaAl <elmahraouidouaa123@gmail.com>
2024-09-17 22:52:04 +0100

    Commit

--- src/actions/socket.ts
+++ src/actions/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { getUserIdServer } from "./users";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://edujsgamessocket.vercel.app/");
 
 export const logInUserSocket = async() =>{
     let result = await getUserIdServer();
