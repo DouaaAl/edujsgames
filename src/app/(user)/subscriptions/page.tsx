dd55cdf563681000778bcc3e8f0f3ea5252cd53d
DouaaAl <elmahraouidouaa123@gmail.com>
2024-09-17 20:41:36 +0100

    commit

--- src/app/(user)/subscriptions/page.tsx
+++ src/app/(user)/subscriptions/page.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import styles from "./page.module.css"
 import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation' 
 
-const page = async() => {
-
+const Page = async () => {
   const plans = [
-      {
-        link: "https://buy.stripe.com/test_00g0372Np7rKeGs7ss",
-        priceId: "prod_Qj3q4ZPS6BLmtP"
-      },
-      {
-        link: "https://buy.stripe.com/test_aEUg2587JcM4gOAbIJ",
-        priceId: "prod_Qj3sk0Nsf3jpys"
-      },
-      {
-        link: "https://buy.stripe.com/test_28o9DH87J6nGaqc002",
-        priceId: "prod_Qj3tdTgeGwSXhb"
-      }
+    {
+      link: "https://buy.stripe.com/test_00g0372Np7rKeGs7ss",
+      priceId: "prod_Qj3q4ZPS6BLmtP"
+    },
+    {
+      link: "https://buy.stripe.com/test_aEUg2587JcM4gOAbIJ",
+      priceId: "prod_Qj3sk0Nsf3jpys"
+    },
+    {
+      link: "https://buy.stripe.com/test_28o9DH87J6nGaqc002",
+      priceId: "prod_Qj3tdTgeGwSXhb"
+    }
   ]
 
-  let user = await currentUser();
+  let user = await currentUser()
 
+  if (!user) {
+    return redirect('/login')
+  }
 
   return (
     <main className={styles.main}>
@@ -28,58 +31,64 @@ const page = async() => {
       <div className={styles.cards}>
         <div className={styles.midCard + ' ' + styles.card}>
           <div>
-            <h1 className={styles.price}> <span className={styles.num}>$6.99/</span> <br />Month</h1>
+            <h1 className={styles.price}>
+              <span className={styles.num}>$6.99/</span> <br />Month
+            </h1>
             <span className={styles.date}>1 Month</span>
           </div>
-            <ul>
-            <li><h4>Acess to All subscription<br />Games</h4></li>              
-            <li><h4>Acess to Time constraints</h4></li>
-            <li><h4>Acess to Adding Study <br />Materials</h4></li>
-            </ul>
-            <a href={
-              plans[1].link + '?prefilled_email='
-              + user?.primaryEmailAddress?.emailAddress
-            }>
-              <button>Subscribe</button>
-            </a>        </div>
+          <ul>
+            <li><h4>Access to All subscription<br />Games</h4></li>              
+            <li><h4>Access to Time Constraints</h4></li>
+            <li><h4>Access to Adding Study <br />Materials</h4></li>
+          </ul>
+          <a href={
+            plans[1].link + '?prefilled_email='
+            + user?.primaryEmailAddress?.emailAddress
+          }>
+            <button>Subscribe</button>
+          </a>        
+        </div>
         <div className={styles.mainCard + ' ' + styles.card}>
           <div>
-            <h1 className={styles.price}><span className={styles.num}>$4.99/</span><br />Month</h1>
+            <h1 className={styles.price}>
+              <span className={styles.num}>$4.99/</span><br />Month
+            </h1>
             <span className={styles.date}>12 Months</span>
           </div>
-            <ul>
-            <li><h4>Acess to All subscription<br />Games</h4></li>              
-            <li><h4>Acess to Time Constraints</h4></li>
-            <li><h4>Acess to Adding Study <br />Materials</h4></li>
-            </ul>
-            <a href={
-              plans[0].link + '?prefilled_email='
-              + user?.primaryEmailAddress?.emailAddress
-            }>
-              <button>Subscribe</button>
-            </a>
+          <ul>
+            <li><h4>Access to All subscription<br />Games</h4></li>              
+            <li><h4>Access to Time Constraints</h4></li>
+            <li><h4>Access to Adding Study <br />Materials</h4></li>
+          </ul>
+          <a href={
+            plans[0].link + '?prefilled_email='
+            + user?.primaryEmailAddress?.emailAddress
+          }>
+            <button>Subscribe</button>
+          </a>
         </div>
-        <div className={styles.smCard+ ' ' + styles.card}>
+        <div className={styles.smCard + ' ' + styles.card}>
           <div>
-            <h1 className={styles.price}><span className={styles.num}>$2.99/</span><br />Month</h1>
+            <h1 className={styles.price}>
+              <span className={styles.num}>$2.99/</span><br />Month
+            </h1>
             <span className={styles.date}>12 Months</span>
           </div>
-            <ul>
-            <li><h4>Acess to All subscription<br />Games</h4></li> 
-            </ul>      
-            <a href={
-              plans[2].link + '?prefilled_email='
-              + user?.primaryEmailAddress?.emailAddress
-            }>
-              <button>Subscribe</button>
-            </a>
+          <ul>
+            <li><h4>Access to All subscription<br />Games</h4></li> 
+          </ul>      
+          <a href={
+            plans[2].link + '?prefilled_email='
+            + user?.primaryEmailAddress?.emailAddress
+          }>
+            <button>Subscribe</button>
+          </a>
         </div>
       </div>
       <div className={styles.game}>
-
       </div>
     </main>
   )
 }
 
-export default page
+export default Page
